45a143535e7853e574de6799bbb094d20ed33501
raphaelprdy <parvedyraphael@gmail.com>
2022-02-07 10:36:18 +0100

    bien joué raph

--- main.py
+++ main.py
@@ -5,25 +5,25 @@ from settings import *
 
 # to force push : git push -f origin branch_name (our branch name is name)
 
-#intialize pygame
-pygame.init()
-pygame.mixer.init()
-clock = pygame.time.Clock()
-
-#create the screen
-#screen = pygame.display.set_mode((1920,1080))
-screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN) #Full screen for many type of screen
-#screen = pygame.display.set_mode((WIDTH,HEIGHT)) #Adjustable in Settings.py
-
-
-#Title and Icon
-pygame.display.set_caption("Age of Empire: Homemade Edition")
-icon = pygame.image.load(os.path.join(common_path,'icon.png'))
-pygame.display.set_icon(icon)
-game = g(screen, clock)
 
 def main():
+    print("test")
+    #intialize pygame
+    pygame.init()
+    pygame.mixer.init()
+    clock = pygame.time.Clock()
 
+    #create the screen
+    #screen = pygame.display.set_mode((1920,1080))
+    screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN) #Full screen for many type of screen
+    #screen = pygame.display.set_mode((WIDTH,HEIGHT)) #Adjustable in Settings.py
+
+
+    #Title and Icon
+    pygame.display.set_caption("Age of Empire: Homemade Edition")
+    icon = pygame.image.load(os.path.join(common_path,'icon.png'))
+    pygame.display.set_icon(icon)
+    game = g(screen, clock)
 
 #Exit game
     running = True
